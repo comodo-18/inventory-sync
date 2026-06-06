e80f0ec53f192d41f4c42afe9ae955e6bb32070e
comodo-18 <jonty.rhodes24@gmail.com>
2026-06-06 23:30:24 +0530

    feat: added redisson timeout config

--- src/main/java/com/anurag/inventory_sync/config/RedissonConfig.java
+++ src/main/java/com/anurag/inventory_sync/config/RedissonConfig.java
@@ -30,7 +30,10 @@ public class RedissonConfig {
                 .setAddress(address)
                 .setPassword(redisPassword.isEmpty() ? null : redisPassword)
                 .setConnectionPoolSize(2)
-                .setConnectionMinimumIdleSize(1);
+                .setConnectionMinimumIdleSize(1)
+                .setConnectTimeout(10000)
+                .setTimeout(5000)
+                .setRetryAttempts(3);
 
         return Redisson.create(config);
     }
